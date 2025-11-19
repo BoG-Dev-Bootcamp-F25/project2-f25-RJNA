b372c0fb6ea771e9aa68f6a6648ddb111f863bc3
Nathan Nguyen <nnguyen11262004@gmail.com>
2025-11-18 19:12:33 -0500

    slight style changes

--- src/app/create-account/page.tsx
+++ src/app/create-account/page.tsx
@@ -1,16 +1,14 @@
 import React from "react";
 import CreateAccountForm from "@/components/CreateAccountForm";
 
-// This is a Server Component.
 export default function CreateAccountPage() {
   return (
-    // Outer container for the entire page
     <div className="min-h-screen flex flex-col bg-gray-50 relative overflow-hidden">
-      {/* Top Header/Navbar */}
       <header className="w-full p-6 bg-white shadow-sm z-10">
-        {/* <div className="max-w-7xl mx-auto">
-          <Logo />
-        </div> */}
+        <div className="max-w-7xl text-black text-5xl font-sans font-semibold flex flex-row items-center gap-3">
+          <div className="w-16 h-12 aspect-square bg-red-600 rounded-2xl"></div>
+          Progress
+        </div>
       </header>
 
       <main className="grow flex items-center justify-center p-4">
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,10 +5,13 @@ export default function LoginPage() {
   return (
     <div className="min-h-screen flex flex-col bg-gray-50 relative overflow-hidden">
       <header className="w-full p-6 bg-white shadow-sm z-10">
-        <div className="max-w-7xl mx-auto"></div>
+        <div className="max-w-7xl text-black text-5xl font-sans font-semibold flex flex-row items-center gap-3">
+          <div className="w-16 h-12 aspect-square bg-red-600 rounded-2xl"></div>
+          Progress
+        </div>
       </header>
 
-      <main className="grow flex items-center justify-center p-4">
+      <main className="grow flex items-center justify-center p-4 ">
         <LoginForm />
       </main>
 
